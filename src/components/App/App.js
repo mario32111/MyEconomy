fe44e7eff58f36b36b58460ea92a4f97fa93c77c
mario32111 <marioge44@gmail.com>
2024-09-26 13:06:24 -0600

    configuracion inicial

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RatingExample from '../RatingExample'
+import { AppUI } from './AppUI';
+
+
+function App() {
+  return (
+    <div>
+      <AppUI />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App/AppUI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppUI() {
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export { AppUI };
