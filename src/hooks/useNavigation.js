46d917a6b0b62a6ab5c939342636520d5cb2d52b
mario32111 <marioge44@gmail.com>
2024-09-29 11:51:04 -0600

    chat AI

--- src/hooks/useNavigation.js
+++ src/hooks/useNavigation.js
@@ -3,6 +3,7 @@ import { useNavigate, useRoutes } from 'react-router-dom';
 import Loggin from '../components/Loggin/Loggin';
 import SignUp from '../components/SignUp/SignUp';
 import Home from '../components/Landing/Landing';
+import ChatAI from '../components/ChaiAI/ChatAI';
 
 export const useNavigation = () => {
     const navigate = useNavigate();
@@ -18,6 +19,7 @@ export const useNavigation = () => {
 // Componente para las rutas de la aplicación
 export const AppRoutes = () => {
     return useRoutes([
+        { path: '/prueba', element: <ChatAI /> },
         { path: '/loggin', element: <Loggin /> },
         { path: '/sign-up', element: <SignUp /> },
         { path: '/', element: <Home /> },
