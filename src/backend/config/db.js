04defde849b6f4f2c63d9263f3551778307c3492
mario32111 <marioge44@gmail.com>
2024-09-28 19:00:54 -0600

    se configuro la API para funcionar de forma local

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
-// Configura la conexión a la base de datos
 const db = mysql.createConnection({
     host: 'localhost',      // Dirección IP de tu servidor local (localhost)
     user: 'root',           // Usuario de MySQL por defecto en XAMPP
     password: '',           // Contraseña vacía por defecto en XAMPP (ajusta si has cambiado la contraseña)
-    database: 'myeconomy',  // Nombre de tu base de datos
+    database: 'myeconomy',  // Tu base de datos
     port: 3306,             // Puerto de MySQL (por defecto es 3306)
 });
 
