22f2e966bf7da2c6c3e8a7500749cef6fcd0bb5a
mario32111 <marioge44@gmail.com>
2024-09-30 00:49:24 -0600

    api funcional con endpoints para login

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',      // Dirección IP de tu servidor local (localhost)
+    host: 'serversito.synology.me',      // Dirección IP de tu servidor local (localhost)
     user: 'root',           // Usuario de MySQL por defecto en XAMPP
-    password: '',           // Contraseña vacía por defecto en XAMPP (ajusta si has cambiado la contraseña)
+    password: 'Hola12345!',           // Contraseña vacía por defecto en XAMPP (ajusta si has cambiado la contraseña)
     database: 'myeconomy',  // Tu base de datos
     port: 3306,             // Puerto de MySQL (por defecto es 3306)
 });
