aea6cf8f5ba84f59822c4405f03313ac28a06737
mario32111 <marioge44@gmail.com>
2024-10-01 21:27:37 -0600

    slider de servicios

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Home from './Home/Home';
 import NavbarHome from './NavbarHome/NavbarHome'
 import Footer from './Footer/Footer';
+import Carousel from './Carousel/Carousel'
 
 function Landing() {
     return (
         <>
             <NavbarHome />
             <Home />
+            <div style={{ backgroundColor: '#121212' }}>
+                <Carousel />
+            </div>
             <Footer />
         </>
     );
